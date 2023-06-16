004d1e6f80cb69ecf42a9c9d7a8d3b8fdf7b7434
srimythri1018 <115878123+srimythri1018@users.noreply.github.com>
2023-06-16 15:17:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 function createTable() {
-    //Write your code here
-  
+  // Prompt for number of rows
+  const rows = prompt("Input number of rows");
+  if (rows === null || isNaN(rows) || rows <= 0) {
+    return;
+  }
+
+  // Prompt for number of columns
+  const columns = prompt("Input number of columns");
+  if (columns === null || isNaN(columns) || columns <= 0) {
+    return;
+  }
+
+  const table = document.getElementById("myTable");
+
+  // Clear any existing rows
+  table.innerHTML = "";
+
+  // Create new rows and cells
+  for (let i = 0; i < rows; i++) {
+    const row = document.createElement("tr");
+    for (let j = 0; j < columns; j++) {
+      const cell = document.createElement("td");
+      cell.textContent = `Row-${i} Column-${j}`;
+      row.appendChild(cell);
+    }
+    table.appendChild(row);
+  }
 }
